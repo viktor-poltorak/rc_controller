7c6229c825a42c3dd5e24922146126602b42bb78
viktor <viktor.poltorak@gmail.com>
2017-07-31 18:59:35 +0300

    process commands

--- connector/connector.ino
+++ connector/connector.ino
@@ -1,10 +1,24 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <WiFiClient.h>
+#include <Servo.h>
 
-const char *ssid = "";
-const char *password = "";
+//network
+const char *ssid = "MainHomeNet";
+const char *password = "piramida321";
 const unsigned int udpPort = 5555; //local port to listen for UDP packets
+
+//Servo
+int STEERING_PIN = D1;    //Arduino 9 or GPIO5
+int ACCELERATOR_PIN = D2; //Aduino 8 or GPIO4
+
+int STEERING_MID = 1500;
+int ACCELERATOR_MID = 1500;
+int acceleratorVal = 0;
+int steeringVal = 0;
+
+Servo wheelServo;
+Servo acceleratorServo;
 
 struct Controller
 {
@@ -22,7 +36,10 @@ void setup()
     delay(10);
     // prepare GPIO2
     pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, 1); //Switch led off
+    digitalWrite(LED_BUILTIN, 0); //Switch led off
+
+    wheelServo.attach(STEERING_PIN);
+    // acceleratorServo.attach(ACCELERATOR_PIN);
 
     //Connect to WiFi network
     Serial.println();
@@ -45,35 +62,54 @@ void setup()
     Udp.begin(udpPort);
 
     controller = findController(WiFi.localIP());
-
+    connectToClient();
     delay(100);
 }
 
-void loop()
+void connectToClient()
 {
-
     if (!client.connected())
     {
         Serial.println("Connecting to controller:");
         Serial.print(controller.ip);
+        Serial.print(':');
         Serial.println(controller.port);
 
         if (client.connect(controller.ip, controller.port))
         {
             Serial.println("connected");
-            client.println("OK");
+            client.print("Im ready");
+        }
+    }
+}
+
+void loop()
+{
+    if (client.available() > 0)
+    {
+        String command = client.readStringUntil(';');
+        command.trim();
+
+        Serial.print("Command: ");
+        Serial.println(command);
+        //Serial.print("Recieved string: ");
+        if (command.length() > 0)
+        {
+            client.print(command);
+            processCommand(command);
         }
     }
 
-    if (client.available() > 0)
+    if (!client.connected())
     {
-        String data = client.readString();
-        Serial.print("Recieved string: ");
-        Serial.println(data);
-        client.println("OK");
+        reset();
+        Serial.println("Client disconnected");
+        Serial.println("connecting...");
+        Serial.println(controller.ip);
+        Serial.println(controller.port);
+        connectToClient();
+        delay(1000);
     }
-
-    delay(3000);
 }
 
 //Search controller server
@@ -101,12 +137,22 @@ Controller findController(IPAddress curIP)
         //receive response from server, it will be HELLO WORLD
         if (Udp.read(buffer, 50) > 0)
         {
+            //Parse TCP:TP:port
+            String data = buffer;
+
+            String ip = getValue(data, ':', 1);
+            String port = getValue(data, ':', 2);
+
             Serial.print("Controller found: ");
-            Serial.print(Udp.remoteIP());
+            Serial.print(ip);
             Serial.print(":");
-            Serial.println(Udp.remotePort());
-            cntr.ip = Udp.remoteIP();
-            cntr.port = Udp.remotePort();
+            Serial.println(port);
+            Serial.println(buffer);
+
+            IPAddress ip_addr;
+            ip_addr.fromString(ip);
+            cntr.ip = ip_addr;
+            cntr.port = port.toInt();
             isServer = true;
         }
         //Wait for 1 second
@@ -114,4 +160,131 @@ Controller findController(IPAddress curIP)
     }
 
     return cntr;
+}
+
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = {0, -1};
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++)
+    {
+        if (data.charAt(i) == separator || i == maxIndex)
+        {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i + 1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void processCommand(String command)
+{
+    //wheelServo.writeMicroseconds(angle);
+    if (command == "state")
+    {
+        printState();
+        return;
+    }
+
+    if (command == "brake")
+    {
+        brake();
+        return;
+    }
+
+    int pos = command.indexOf(':');
+    if (pos != 0)
+    {
+        String subCommand = getValue(command, ':', 0);
+        String value = getValue(command, ':', 1);
+        processSubCommand(subCommand, value);
+    }
+}
+
+void processSubCommand(String subCommand, String value)
+{
+    Serial.print("Sub command: ");
+    Serial.print(subCommand);
+    Serial.print(" = ");
+    Serial.println(value);
+
+    if (subCommand == "AM")
+    {
+        ACCELERATOR_MID = value.toInt();
+    }
+    if (subCommand == "SM")
+    {
+        STEERING_MID = value.toInt();
+    }
+    if (subCommand == "A")
+    {
+        setAccelerator(value.toInt());
+    }
+    if (subCommand == "S")
+    {
+        setSteering(value.toInt());
+    }
+}
+
+void brake()
+{
+    //If car moving forward
+    if (acceleratorVal > ACCELERATOR_MID)
+    {
+        acceleratorVal = ACCELERATOR_MID - 300;
+    }
+    //If car moving backward
+    if (acceleratorVal < ACCELERATOR_MID)
+    {
+        acceleratorVal = ACCELERATOR_MID + 100;
+    }
+
+    acceleratorServo.writeMicroseconds(acceleratorVal);
+    delayMicroseconds(500);
+    //Reset accelerator val
+    acceleratorVal = ACCELERATOR_MID;
+    acceleratorServo.writeMicroseconds(acceleratorVal);
+}
+
+void reset()
+{
+    brake();
+    setSteering(STEERING_MID);
+}
+
+void setAccelerator(int value)
+{
+    acceleratorVal = value;
+    acceleratorServo.writeMicroseconds(acceleratorVal);
+}
+
+void setSteering(int value)
+{
+    steeringVal = value;
+    wheelServo.writeMicroseconds(steeringVal);
+}
+
+void printState()
+{
+    Serial.println("Current state:");
+    Serial.print("ACCELERATOR_MID: ");
+    Serial.println(ACCELERATOR_MID);
+    Serial.print("STEERING_MID: ");
+    Serial.println(STEERING_MID);
+    Serial.print("steering value: ");
+    Serial.println(steeringVal);
+    Serial.print("accelerator value: ");
+    Serial.println(acceleratorVal);
+
+    client.print("ACCELERATOR_MID: ");
+    client.print(ACCELERATOR_MID);
+    client.print("STEERING_MID: ");
+    client.print(STEERING_MID);
+    client.print("steering value: ");
+    client.print(steeringVal);
+    client.print("accelerator value: ");
+    client.print(acceleratorVal);
 }
